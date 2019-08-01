954babcebfaab3d9ddf0611974f0ab029ac1ab9c
andrey0902 <andreypavlov1706@gmail.com>
2019-08-02 00:03:45 +0300

    234234234

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,3 +6,7 @@ tar -czf package.tgz ../dist/travis && \
 ssh -p54231 usr01@188.166.54.169 "rm -rf /var/www/light-it-08.tk/* "&& \
 scp -P54231 package.tgz usr01@188.166.54.169:/var/www/light-it-08.tk/ && \
 ssh -p54231 usr01@188.166.54.169 "tar -xvf package.tgz"
+tar -czf package.tgz ../dist/travis && \
+ssh -p54231 usr01@188.166.54.169 "rm -rf /var/www/light-it-08.tk/* "&& \
+scp -P54231 package.tgz usr01@188.166.54.169:/var/www/light-it-08.tk/ && \
+ssh -p54231 usr01@188.166.54.169 "tar -xvf package.tgz"
