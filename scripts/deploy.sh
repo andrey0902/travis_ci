880582a6ee99f512478ee65e2dc6526c343dddf8
andrey0902 <andreypavlov1706@gmail.com>
2019-08-03 20:21:03 +0300

    add script 16

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,3 +5,6 @@ ssh -p$PORT $REMOTEUSER@$REMOTEHOST "rm -rf /var/www/light-it-08.tk/*"
 #tar -czf package.tgz ./dist/travis && \
 #scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
 #ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
+
+
+
