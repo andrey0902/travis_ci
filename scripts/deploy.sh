37cc773519bac3c8eeadb0f9b65b137d4b530079
andrey0902 <andreypavlov1706@gmail.com>
2019-08-03 21:40:32 +0300

    add script 27

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 set -x
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p$PORT $REMOTEUSER@$REMOTEHOST "rm -rf /var/www/light-it-08.tk/*"
-
-#tar -czf package.tgz ./dist/travis && \
-#scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
-#ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p$PORT $REMOTEUSER@$REMOTEHOST "rm -rf /var/www/light-it-08.tk/*" && \
+tar -czf package.tgz ./dist/travis && \
+scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p$PORT $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
 
