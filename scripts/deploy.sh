425ac1c363acf925c319c04fc8c07cf1ebe2ae04
andrey0902 <andreypavlov1706@gmail.com>
2019-08-03 18:58:51 +0300

    add script 7

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 set -x
-echo "hello world $REMOTEAPPDIR $REMOTEUSER $REMOTEHOST $PORT" && \
-tar -czf package.tgz ./dist/travis && \
-scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
-ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
+ssh -o "StrictHostKeyChecking=no" -p$PORT $REMOTEUSER@$REMOTEHOST "rm -rf /var/www/light-it-08.tk/*"
+
+#tar -czf package.tgz ./dist/travis && \
+#scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
+#ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
