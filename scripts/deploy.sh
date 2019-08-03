b909cf6e3f4b688ee002b64826f29dbb49635b51
andrey0902 <andreypavlov1706@gmail.com>
2019-08-03 21:08:10 +0300

    add script 21

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -x
-ssh -p$PORT $REMOTEUSER@$REMOTEHOST "rm -rf /var/www/light-it-08.tk/*"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p$PORT $REMOTEUSER@$REMOTEHOST "rm -rf /var/www/light-it-08.tk/*"
 
 #tar -czf package.tgz ./dist/travis && \
 #scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
