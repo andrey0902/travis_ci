166180aff25159421578304cfc2f61b423aae839
andrey0902 <andreypavlov1706@gmail.com>
2019-08-03 18:18:35 +0300

    add script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env sh
-
 set -x
 
-tar -czf package.tgz ../dist/travis && \
-ssh -p54231 usr01@188.166.54.169 "rm -rf /var/www/light-it-08.tk/* "&& \
-scp -P54231 package.tgz usr01@188.166.54.169:/var/www/light-it-08.tk/ && \
-ssh -p54231 usr01@188.166.54.169 "tar -xvf package.tgz"
-tar -czf package.tgz ../dist/travis && \
-ssh -p54231 usr01@188.166.54.169 "rm -rf /var/www/light-it-08.tk/* "&& \
-scp -P54231 package.tgz usr01@188.166.54.169:/var/www/light-it-08.tk/ && \
-ssh -p54231 usr01@188.166.54.169 "tar -xvf package.tgz"
+tar -czf package.tgz ./dist/travis && \
+scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR
