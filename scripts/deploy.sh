4f4a96274048834de316d4328b3ce1fbdbb37e4e
andrey0902 <andreypavlov1706@gmail.com>
2019-08-03 22:09:44 +0300

    add script 36

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,5 @@
 set -x
 #ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p$PORT $REMOTEUSER@$REMOTEHOST "rm -rf /var/www/light-it-08.tk/*" && \
 tar -czf package.tgz ./dist/travis && \
-scp -p54231 package.tgz usr01@188.166.54.169:/var/www/light-it-08.tk/
-#&& \
-#ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no --p$PORT $REMOTEUSER@$REMOTEHOST 'bash -s' < ./scripts/untar.sh
+scp -P 54231 package.tgz usr01@188.166.54.169:/var/www/light-it-08.tk/
 
-#- echo "hello world $REMOTEAPPDIR $REMOTEUSER $REMOTEHOST $PORT"
-#- ssh -o "StrictHostKeyChecking=no" -p$PORT $REMOTEUSER@$REMOTEHOST "ls -la"
-#- tar -czf package.tgz ./dist/travis
-#- ssh -o "StrictHostKeyChecking=no" -p$PORT $REMOTEUSER@$REMOTEHOST "rm -rf /var/www/light-it-08.tk/*"
-#- scp -P$PORT package.tgz $REMOTEUSER@$REMOTEHOST:/var/www/light-it-08.tk/
-#- ssh -p$PORT $REMOTEUSER@$REMOTEHOST "cd /var/www/light-it-08.tk/ && tar -xvf /var/www/light-it-08.tk/package.tgz"
-#- ssh -o "StrictHostKeyChecking=no" -p$PORT $REMOTEUSER@$REMOTEHOST "ls -la /var/www/light-it-08.tk/"
