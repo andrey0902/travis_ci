57a4b8348101e730f02f0db40cee9d1efb2bb17e
andrey0902 <andreypavlov1706@gmail.com>
2019-08-03 18:49:43 +0300

    add script 5

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,5 @@ set -x
 
 tar -czf package.tgz ./dist/travis && \
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
-ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
+ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh && \
+echo "hello world $REMOTEAPPDIR $REMOTEUSER $REMOTEHOST $PORT"
