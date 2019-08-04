3e53cbf143f5df88501969e72d029cec7be920a0
andrey0902 <andreypavlov1706@gmail.com>
2019-08-04 17:48:23 +0300

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -x
-#ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p$PORT $REMOTEUSER@$REMOTEHOST "rm -rf /var/www/light-it-08.tk/*" && \
-tar -czf package.tgz ./dist/travis && \
-scp -P 54231 package.tgz usr01@188.166.54.169:/var/www/light-it-08.tk/
 
+tar -czf package.tgz ./dist/travis;
+scp -P 54231 package.tgz usr01@188.166.54.169:/var/www/light-it-08.tk/;
+ssh -o "StrictHostKeyChecking=no" -p"$PORT" "$REMOTEUSER"@"$REMOTEHOST" "rm -rf /var/www/light-it-08.tk/*";
+scp -P"$PORT" package.tgz "$REMOTEUSER"@"$REMOTEHOST":/var/www/light-it-08.tk/;
+ssh -p"$PORT" "$REMOTEUSER"@"$REMOTEHOST" "cd /var/www/light-it-08.tk/ && tar -xvf /var/www/light-it-08.tk/package.tgz";
